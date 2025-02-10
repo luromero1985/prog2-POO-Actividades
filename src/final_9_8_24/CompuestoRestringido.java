812fccda8061fbdee566af9d82074cf9e440c083
Luciana Yael Romero <lu.yael.r@gmail.com>
2025-02-10 10:38:52 -0300

    corregi el metodo add de la clase compuestoRestringido

--- src/final_9_8_24/CompuestoRestringido.java
+++ src/final_9_8_24/CompuestoRestringido.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 public class CompuestoRestringido extends Compuesto{
 
 	private Filtro filtro; // strategy
-	private ArrayList<Menu> menues;
+	
 	
 	public CompuestoRestringido(String nombre, Filtro f) {
 		super(nombre);
 		this.filtro = f;
-		this.menues = new ArrayList<>();
+
 	}
 	
 	
@@ -21,11 +21,11 @@ public class CompuestoRestringido extends Compuesto{
 	public void setFiltro(Filtro f) {
 		this.filtro = f;
 	}
-
 	
-	public void addMenu(Menu m) {
-		if (!this.menues.contains(m) && this.filtro.cumple(m) ) {
-			menues.add(m);
-			}
-}
+	   @Override
+	    public void addMenu(Menu m) {
+	        if (this.filtro.cumple(m)) {
+	            super.addMenu(m); 
+	        }
+	    }
 }
--- src/final_9_8_24/Main.java
+++ src/final_9_8_24/Main.java
@@ -3,8 +3,31 @@ package final_9_8_24;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		CasaComidas casa =new CasaComida();
+		
+		Filtro filtro2=new FiltroContieneIngrediente("sal");
+		Filtro filtro1=new FiltroPrecioMenor(5000);
+		
+		Cliente c1=new Cliente("Diego", 500,filtro1);
+		Cliente c2= new Cliente("Ana", 150,filtro2);
+		
+		Comida comida1= new Comida ("comida1", 40, 4500.0, 350);
+		Comida comida2= new Comida ("comida2", 65, 6000.0, 500);
+		Comida comida3= new Comida ("comida3", 20, 1500.0, 150);
+		
+		Compuesto menu= new Compuesto();
+		CompuestoRestringido menuRestringido= new CompuestoRestringido(filtro1);
+		
+		menu.addMenu(comida1);
+		menu.addMenu(comida2);
+		menu.addMenu(comida3);
+		
+		menuRestringido.addMenu(comida1);
+		menuRestringido.addMenu(comida2);
+		menuRestringido.addMenu(comida3);
+		
+		
 	}
 
 }
